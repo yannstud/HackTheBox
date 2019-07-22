ed726ee9adf5859fef96ada9919e7701b79a7433
floki <ysalaun@student.42.fr>
2019-07-22 09:35:02 +0200

    writeup machine added tools folder created

---
+++ swagshop/checkout.sql
@@ -0,0 +1,14 @@
+POST /index.php/checkout/cart/estimatePost/ HTTP/1.1
+Host: 10.10.10.140
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3
+Accept-Encoding: gzip, deflate
+Referer: http://10.10.10.140/index.php/checkout/cart/
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 60
+Connection: close
+Cookie: frontend=emia861p594vi5evd902cfg0k7; external_no_cache=1
+Upgrade-Insecure-Requests: 1
+
+country_id=TZ&region_id=&region=tama&estimate_postcode=92111
