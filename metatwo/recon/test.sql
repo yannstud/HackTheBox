2b001e063fb4bb60ef33fcd7edf378aa119f0aab
floky <ysalaun@student.42.fr>
2022-11-14 16:39:17 +0100

    some more machines

---
+++ metatwo/recon/test.sql
@@ -0,0 +1,14 @@
+POST /wp-admin/admin-ajax.php HTTP/1.1
+Host: metapress.htb
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0
+Accept: application/json, text/plain, */*
+Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3
+Accept-Encoding: gzip, deflate
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 993
+Origin: http://metapress.htb
+Connection: close
+Referer: http://metapress.htb/events/
+Cookie: PHPSESSID=6teh8v8hse2pcbe56bm10fbvu6; wordpress_test_cookie=WP%20Cookie%20check
+
+action=bookingpress_before_book_appointment&appointment_data%5Bselected_category%5D=1&appointment_data%5Bselected_cat_name%5D=Events&appointment_data%5Bselected_service%5D=1&appointment_data%5Bselected_service_name%5D=Startup%20meeting&appointment_data%5Bselected_service_price%5D=%240.00&appointment_data%5Bservice_price_without_currency%5D=0&appointment_data%5Bselected_date%5D=2022-11-11&appointment_data%5Bselected_start_time%5D=11%3A30&appointment_data%5Bselected_end_time%5D=12%3A00&appointment_data%5Bcustomer_name%5D=&appointment_data%5Bcustomer_firstname%5D=toto&appointment_data%5Bcustomer_lastname%5D=tata&appointment_data%5Bcustomer_phone%5D=qwe&appointment_data%5Bcustomer_email%5D=toto%40atat.com&appointment_data%5Bappointment_note%5D=qwe&appointment_data%5Bselected_payment_method%5D=&appointment_data%5Bcustomer_phone_country%5D=US&appointment_data%5Btotal_services%5D=&appointment_data%5Bstime%5D=1667583059&appointment_data%5Bspam_captcha%5D=ZDpMii9V0dWa&_wpnonce=f1c5d884ac
